ae34c7fba36581ada05cf039882a84253e5a94b7
David Alves Dutra <dadutra@hotmail.com>
2025-04-22 08:29:20 -0300

    Adicionado endpoints para busca por TSID e findAll paginado.

--- src/main/java/com/dutra/sensors/management/api/controllers/SensorController.java
+++ src/main/java/com/dutra/sensors/management/api/controllers/SensorController.java
@@ -4,9 +4,14 @@ import com.dutra.sensors.management.api.model.SensorInput;
 import com.dutra.sensors.management.api.model.SensorOutput;
 import com.dutra.sensors.management.common.IdGenerator;
 import com.dutra.sensors.management.domain.model.Sensor;
+import com.dutra.sensors.management.domain.model.SensorId;
 import com.dutra.sensors.management.domain.repository.SensorRepository;
+import io.hypersistence.tsid.TSID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/sensors")
@@ -17,6 +22,21 @@ public class SensorController {
     public SensorController(SensorRepository repository) {
         this.repository = repository;
     }
+
+    @GetMapping("{sensorId}")
+    public SensorOutput getOne(@PathVariable TSID sensorId) {
+        Sensor sensor = repository.findById(new SensorId(sensorId))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        return new SensorOutput(sensor);
+    }
+
+    @GetMapping
+    public Page<SensorOutput> findAll(Pageable pageable) {
+        Page<Sensor> sensors = repository.findAll(pageable);
+        return sensors.map(SensorOutput::new);
+    }
+
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
